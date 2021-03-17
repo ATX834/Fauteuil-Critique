b834597279eaaa0cefdb46d4fda5eabffc34c0a8
ATX834 <clementbacquet@hotmail.fr>
2021-03-17 12:22:25 +0100

    Update aboutme.php

--- aboutme.php
+++ aboutme.php
@@ -3,9 +3,7 @@ include 'header.php';
 // ---------------------------content------------------------------
 echo "<p> Esse pariatur non tempor esse nostrud incididunt amet consectetur aute incididunt. 
 Eiusmod amet ullamco ipsum occaecat dolor amet aliquip. Elit non nostrud et magna magna duis nostrud nisi cupidatat. 
-Fugiat excepteur tempor mollit exercitation. Nulla fugiat tempor nisi ipsum laboris.";
-
-
+Fugiat excepteur tempor mollit exercitation. Nulla fugiat tempor nisi ipsum laboris.</p>";
 
 // ----------------------------end of content-----------------------------
 include 'footer.php';
