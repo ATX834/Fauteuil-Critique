02590f8d795ba9d65cd4dc0dc693a962710b2ac2
Pierre-Jean Touraille <pierre-jean-touraille_student2021@wilder.school>
2021-03-17 19:05:49 +0100

    premiere mise en place du menu deroulant

--- navbar.php
+++ navbar.php
@@ -1,10 +1,27 @@
 <nav>
-    <!-- <div class="burger">
-        <button class="button-burger">
-            <img id='burger-menu' src="/source/burger-menu-icon.jpg" 
-            alt="burger-menu" width="100%" height="100%">
-        </button>
-    </div> -->
+
+    <div class="burger">
+        <div>
+            <button class="button-burger">
+                <img id='burger-menu' src="/source/burger-menu-icon.jpg" alt="burger-menu" width="100%" height="100%">
+            </button>
+        </div>
+        <div class="burger-click">
+            <ul class="burger-list">
+                <li>
+                    <a href="index.php"> Index
+                    </a>
+                </li>
+                <li><a href="reviews.php"> Reviews
+                    </a></li>
+                <li><a href="aboutme.php"> A propos
+                    </a></li>
+                <li><a href="form.php"> Contact
+                    </a></li>
+            </ul>
+        </div>
+    </div>
+
 
     <div class="logo-mobile">
         <a href="index.php">
@@ -12,19 +29,19 @@
         </a>
     </div>
 
-    <div class="menu">
+    <!-- <div class="menu">
         <div class="home">
             <a href="index.php">
                 <img id='logo' src="/source/home-icon.png" alt="home-icon" width="50px" height="50px">
             </a>
         </div>
-
+        
         <div class="review">
             <a href="reviews.php">
                 <h1> Reviews </h1>
             </a>
         </div>
-
+        
         <div class="about">
             <a href="aboutme.php">
                 <h1>A propos</h1>
@@ -35,7 +52,7 @@
                 <h1>Contact</h1>
             </a>
         </div>
-    </div>
+    </div> -->
 
     <div class="colormode">
         <label class='switch'>
