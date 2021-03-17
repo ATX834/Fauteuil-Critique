a9bfa1193f1596b226724393d0b10810346ed54d
Pierre-Jean Touraille <pierre-jean-touraille_student2021@wilder.school>
2021-03-17 14:59:29 +0100

    mise à jour du bouton Dmode et menu

--- navbar.php
+++ navbar.php
@@ -1,15 +1,16 @@
 <nav>
     <div class="burger">
         <button class="button-burger">
-            <img id='burger-menu' src="/source/burger-menu-icon.jpg" alt="burger-menu" width="100%" height="100%">
+            <img id='burger-menu' src="/source/burger-menu-icon.jpg" 
+            alt="burger-menu" width="100%" height="100%">
         </button>
     </div>
 
     <div class="logo-mobile">
-        <img src="/source/logo.png" alt="logo" width="80px" height="80px">
+        <img src="/source/logo.png" alt="logo" width="80px" height="100%">
     </div>
 
-    <div class="menu, hidden">
+    <!-- <div class="menu, hidden">
         <div class="home">
             <img id='logo' src="/source/home-icon.png" alt="home-icon" width="100px" height="100px">
         </div>
@@ -24,13 +25,14 @@
         <div class="form">
             <a href="form.php">Contact</a>
         </div>
-    </div>
+    </div> -->
 
     <div class="colormode">
         <label class='switch'>
             <input type="checkbox">
             <span class="slider round"></span>
         </label>
-        <img src="/source/disco-ball.png" alt="disco-ball" width="120px" height="100px">
+        <img src="/source/disco-ball.png" alt="disco-ball" 
+        width="80px" height="80px">
     </div>
 </nav>
