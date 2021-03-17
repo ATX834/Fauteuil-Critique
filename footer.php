f0974cf009ff2dd4ef02941b00e79615bdb7deea
ATX834 <clementbacquet@hotmail.fr>
2021-03-17 11:14:33 +0100

    Déplacement logo réseaux sociaux dans source et correction src dans footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
   <footer>
-    <img class="socialNetworkLogo" src="logo-youtube.png" alt= "Bouton Youtube"width="50px" height="50px">
-    <img class="socialNetworkLogo" src="logo-twitter.png" alt= "Bouton Twitter" width="50px" height="50px">
-    <img class="socialNetworkLogo" src="logo-facebook.png" alt= "Bouton Facebook"width="50px" height="50px">
-    <img class="socialNetworkLogo" src="logo-insta.png" alt= "Bouton Instagram" width="50px" height="50px">
+    <img class="socialNetworkLogo" src="source/logo-youtube.png" alt= "Bouton Youtube"width="50px" height="50px">
+    <img class="socialNetworkLogo" src="source/logo-twitter.png" alt= "Bouton Twitter" width="50px" height="50px">
+    <img class="socialNetworkLogo" src="source/logo-facebook.png" alt= "Bouton Facebook"width="50px" height="50px">
+    <img class="socialNetworkLogo" src="source/logo-insta.png" alt= "Bouton Instagram" width="50px" height="50px">
     <p>
       <a href="lien-page-contact">Contact</a>
     </p>
