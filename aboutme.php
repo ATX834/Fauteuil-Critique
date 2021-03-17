3810b51342cb1e2be4f2507594e8c2abd5103343
PierreCapel <pecapel@gmail.com>
2021-03-17 16:13:17 +0100

    added picture

--- aboutme.php
+++ aboutme.php
@@ -1,7 +1,9 @@
 <?php
 include 'header.php' ?>
 <!-- // ---------------------------content------------------------------ -->
-<div id=aboutMeText>
+<div id = aboutMeBody>
+<img src="source/mypicture.jpg" alt="Picture of me" id=pictureOfMe>
+<div id= aboutMeText>
     <p> Je suis journaliste de profession et né dans le Val d’Oise le 6 Avril 1983.
         Mon père était conducteur de trains et ma mère mécanicien automobile ce qui m’a donné très tôt le goût pour les voyages.
         Ma passion pour le journalisme m’est venue alors que je regardais Lois et Clark sur le téléviseur Philipps familial,
@@ -15,20 +17,21 @@ include 'header.php' ?>
         dans une caravane en Autriche. Je me suis donc naturellement réorienté vers la coiffure pour dames car j’apprécie leur contact et 
         elles apprécient le mien je crois. Jusqu’au jour ou mes problèmes personnels (principalement ma troisième femme qui me réclamait 
         une pension alimentaire) ont commencé à se faire sentir dans mon travail et m’ont fait louper un chinion important ce qui a précipité
-         ma fuite vers les Maldives. Voulant découvrir le monde comme mes confrères Antoine de Maximy et Bernard De La Villardière, 
-         j’ai pendant un temps vendu des bracelets sur les plages mais je transpirais toujours à grosses goutes et puis un jour un touriste 
-         canadien m’a fait lire un article sur le premier blog de l’histoire et ce fut le déclic. Sans trop tergiverser c’est sur la deuxième 
-         moitié du haut paléolitique du coté de la mer morte que pour la première fois un individu dessina sur un mur un contenu dénonciatoire 
-         à l’encontre du chef du village (une histoire d’adultère que je préfère ne pas développer ici) invitant les autres villageois à réagir 
-         a l’aide de leur pouce et d’un peu de peinture. Le premier blog était né et ce pan de mur est aujourd’hui visible au musée du Skyblog 
-         de Seattle pour 4$ l’entrée avec un hot dog offert (devis familial sur demande). Mais assez parlé de moi, parlons un peu de vous. 
-         Oh et puis non continuons à parler de moi, j’adore le journalisme comme je vous le disais mais je voudrais préciser qu’il reste pour 
-         moi un art mineur, car au fond il ne rend pas les gens heureux et les arts majeurs se doivent d’apporter la joie et la paix entre les 
-         hommes. C’est pourquoi le samedi soir venu j’aime chausser mes chaussures compensées et donner du bonheur aux gens de plus de 40 ans 
-         en boite de nuit avec mon système discomobile . Car voyez-vous dans la vie de tous les jours je suis le grand reporter que vous 
-         conaissez tous mais à la nuit tombée je suis le héros dont cette ville à besoin. Je suis… Disco Lolo.</p>;
+        ma fuite vers les Maldives. Voulant découvrir le monde comme mes confrères Antoine de Maximy et Bernard De La Villardière, 
+        j’ai pendant un temps vendu des bracelets sur les plages mais je transpirais toujours à grosses goutes et puis un jour un touriste 
+        canadien m’a fait lire un article sur le premier blog de l’histoire et ce fut le déclic. Sans trop tergiverser c’est sur la deuxième 
+        moitié du haut paléolitique du coté de la mer morte que pour la première fois un individu dessina sur un mur un contenu dénonciatoire 
+        à l’encontre du chef du village (une histoire d’adultère que je préfère ne pas développer ici) invitant les autres villageois à réagir 
+        a l’aide de leur pouce et d’un peu de peinture. Le premier blog était né et ce pan de mur est aujourd’hui visible au musée du Skyblog 
+        de Seattle pour 4$ l’entrée avec un hot dog offert (devis familial sur demande). Mais assez parlé de moi, parlons un peu de vous. 
+        Oh et puis non continuons à parler de moi, j’adore le journalisme comme je vous le disais mais je voudrais préciser qu’il reste pour 
+        moi un art mineur, car au fond il ne rend pas les gens heureux et les arts majeurs se doivent d’apporter la joie et la paix entre les 
+        hommes. C’est pourquoi le samedi soir venu j’aime chausser mes chaussures compensées et donner du bonheur aux gens de plus de 40 ans 
+        en boite de nuit avec mon système discomobile . Car voyez-vous dans la vie de tous les jours je suis le grand reporter que vous 
+        conaissez tous mais à la nuit tombée je suis le héros dont cette ville à besoin. Je suis… Disco Lolo.</p>
+    </div>
 </div>
-<!-- // ----------------------------end of content----------------------------- -->
-<?php
+    <!-- // ----------------------------end of content----------------------------- -->
+    <?php
 include 'footer.php';
 ?>
