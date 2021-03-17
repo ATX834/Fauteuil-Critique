1827282bda9280e2eac16e467553d4e66a5912f0
Pierre-Jean Touraille <pierre-jean-touraille_student2021@wilder.school>
2021-03-17 12:33:32 +0100

    ajout bouton Dmode

--- navbar.php
+++ navbar.php
@@ -5,25 +5,31 @@
             alt="burger-menu" width="100%" height="100%">
         </button>
     </div>
-
-    <div class="hidden">
-        <div class="logo">
-            <img src="/source/logo.png" alt="logo" width="80px" height="80px">
-        </div>
-
+    
+    <div class="logo-mobile">
+        <img src="/source/logo.png" alt="logo" width="80px" height="80px">
+    </div>
+    
+    <!-- <div class="hidden">
+        
         <div class="menu">
             <div class="home">
-                <img id='logo' src="/source/home-icon.png" alt="home-icon" width="80px" height="80px">
+                <img id='logo' src="/source/home-icon.png" alt="home-icon" width="100px" height="100px">
             </div>
-
+            
             <div class="review">
-            </div>
-
-            <div class="about">
-            </div>
-        </div>
-    </div>
-
+                </div>
+                
+                <div class="about">
+                    </div>
+                </div>
+            </div> -->
+            
     <div class="colormode">
+        <label class='switch'>
+            <input type="checkbox">
+            <span class="slider round"></span>
+        </label>
+        <img src="/source/disco-ball.png" alt="disco-ball" width="120px" height="100px">
     </div>
 </nav>
