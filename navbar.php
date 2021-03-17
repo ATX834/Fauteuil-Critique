3d710f7bd487454e7a80a11786e9df8ae630d942
Pierre-Jean Touraille <pierre-jean-touraille_student2021@wilder.school>
2021-03-17 15:40:19 +0100

    mise à jour dela barre de navigation sur version pc

--- navbar.php
+++ navbar.php
@@ -1,38 +1,47 @@
 <nav>
-    <div class="burger">
+    <!-- <div class="burger">
         <button class="button-burger">
             <img id='burger-menu' src="/source/burger-menu-icon.jpg" 
             alt="burger-menu" width="100%" height="100%">
         </button>
-    </div>
+    </div> -->
 
     <div class="logo-mobile">
-        <a href="index.php"><img src="/source/logo.png" alt="logo" width="80px" height="100%"></a>
+        <a href="index.php">
+            <img src="/source/logo.png" alt="logo" width="80px" height="100%">
+        </a>
     </div>
 
-    <!-- <div class="menu, hidden">
+    <div class="menu">
         <div class="home">
-            <a href="index.php"><img id='logo' src="/source/home-icon.png" alt="home-icon" width="100px" height="100px"></a>
+            <a href="index.php">
+                <img id='logo' src="/source/home-icon.png" alt="home-icon" width="50px" height="50px">
+            </a>
         </div>
 
         <div class="review">
-            <a href="reviews.php">Critiques</a>
+            <a href="reviews.php">
+                <h1> Reviews </h1>
+            </a>
         </div>
-        
+
         <div class="about">
-            <a href="aboutme.php">A propos de moi</a>
+            <a href="aboutme.php">
+                <h1>A propos</h1>
+            </a>
         </div>
         <div class="form">
-            <a href="form.php">Contact</a>
+            <a href="form.php">
+                <h1>Contact</h1>
+            </a>
         </div>
-    </div> -->
+    </div>
 
     <div class="colormode">
         <label class='switch'>
             <input type="checkbox">
             <span class="slider round"></span>
         </label>
-        <img src="/source/disco-ball.png" alt="disco-ball" 
-        width="80px" height="80px">
+        <img src="/source/disco-ball.png" alt="disco-ball" width="80px" height="80px">
     </div>
 </nav>
