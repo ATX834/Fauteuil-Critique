2d31218bf420e2d64d208f7d1943c0ad937a27b8
ATX834 <clementbacquet@hotmail.fr>
2021-03-17 16:04:16 +0100

    changement title

--- header.php
+++ header.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DISCO WILD</title>
     <link rel='stylesheet' href="styles.css" media='all' />
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova&family=Jacques+Francois&display=swap" rel="stylesheet">
 </head>
 
-<body>
+<body style="background-color : #fefee2">
 
     <header>
         <?php include 'navbar.php' ?>
