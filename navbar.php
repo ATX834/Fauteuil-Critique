fcbb7883c9017ee8b84d478a65ee843c7256134f
ATX834 <clementbacquet@hotmail.fr>
2021-03-18 13:33:57 +0100

    correction dans les sources images de la navbar

--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@
     <div class="burger">
         <div style="padding:10% 0 0 10%">
             <button class="button-burger">
-                <img id='burger-menu' src="/source/burger-menu-icon.jpg" alt="burger-menu" width="100%" height="100%">
+                <img id='burger-menu' src="source/burger-menu-icon.jpg" alt="burger-menu" width="100%" height="100%">
             </button>
         </div>
         <div class="burger-click">
@@ -25,23 +25,23 @@
 
     <div class="logo-mobile">
         <a href="index.php">
-            <img src="/source/logo.png" alt="logo" width="80px" height="100%">
+            <img src="source/logo.png" alt="logo" width="80px" height="100%">
         </a>
     </div>
 
     <div class="menu">
         <div class="home">
             <a href="index.php">
-                <img id='logo' src="/source/home-icon.png" alt="home-icon" width="50px" height="50px">
+                <img id='logo' src="source/home-icon.png" alt="home-icon" width="50px" height="50px">
             </a>
         </div>
-        
+
         <div class="review">
             <a href="reviews.php">
                 <h1> Reviews </h1>
             </a>
         </div>
-        
+
         <div class="about">
             <a href="aboutme.php">
                 <h1>A propos</h1>
@@ -59,6 +59,6 @@
             <input type="checkbox">
             <span class="slider round"></span>
         </label>
-        <img src="/source/disco-ball2.png" alt="disco-ball" width="80px" height="80px">
+        <img src="source/disco-ball2.png" alt="disco-ball" width="80px" height="80px">
     </div>
 </nav>
