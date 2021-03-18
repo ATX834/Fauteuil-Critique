ce34d0c80e6370e68a1d7612e21690ce75688eae
Pierre-Jean Touraille <pierre-jean-touraille_student2021@wilder.school>
2021-03-18 15:45:51 +0100

    mise en page du code

--- reviews.php
+++ reviews.php
@@ -1,18 +1,17 @@
 <?php include 'header.php' ?>
-  <content>
-    <div class="categories">
-<?php
-$categoriesArray = ["Jeux Vidéo","Film","Musique"];
-for($i = 0; $i < count($categoriesArray); $i++)
-{
-    echo "<li>$categoriesArray[$i]</li>";
-}
-?>
+<content>
+  <div class="categories">
+    <?php
+    $categoriesArray = ["Jeux Vidéo", "Film", "Musique"];
+    for ($i = 0; $i < count($categoriesArray); $i++) {
+      echo "<li>$categoriesArray[$i]</li>";
+    }
+    ?>
   </div>
   <div class="reviews-card">
     <div> Critiques jeux vidéos</div>
     <div>Critiques filmographiques</div>
     <div>Critiques musicales</div>
-    </div>
-  </content>
+  </div>
+</content>
 <?php include 'footer.php' ?>
