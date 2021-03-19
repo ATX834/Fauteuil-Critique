d650bcbc8367ef529a0d7e20739ad6d41a0e0832
Baptistedupont9094 <baptistedupont9094@gmail.com>
2021-03-19 09:03:11 +0100

    added reviews

--- reviews.php
+++ reviews.php
@@ -27,16 +27,50 @@ for($i = 0; $i < count($categoriesArray); $i++)
        et vous le recommande chaudement.</p>
        </div>
 
+       <div id="card">
+       <img src="source/final_fantasy.jpeg"><br/>
+       Nom: Final Fantasy VII: Remake<br/>
+       Editeur: Square Enix<br/>
+       Date de sortie: 2020<br/>
+       <p>Un remake de la première partie du jeu culte sorti en 1997 qui se concentre sur la partie Midgar avec un peu de réécriture 
+       scénaristique par rapport au premier qui permet de creuser les rapports entre les personnages et suciter de l’interet pour la suite. 
+       Le plaisir de ce balader dans les taudis ou le Wall Market est indéniable, les musiques sont superbement réorchestrées et nos héros 
+       (qui ont désormais une voix et des expressions faciales) sont toujours aussi attachants. Le gameplay est un hybride entre action-RPG et 
+       système de tour à tour comme sur l’original. Les déplacements sont assez lents et la promesse de liberté dans Midgar n’est pas vraiment 
+       tenue tant les niveau sont finalement des suites de couloirs avec  assez peu d’interraction. Bon jeu quand même pour les nostalgiques qui 
+       se termine assez rapidement et nous donne envie de voir la suite.</p>
+       </div>
+
     <h1>Critiques Filmographiques</h1>
       <div id="card">
       <img src="source/choc_titans.jpg"><br/>
-      Nom : Le Choc des Titans<br/>
+      Nom: Le Choc des Titans<br/>
       Réalisateur: Louis Leterrier<br/>
-      Date de sortie : 2010<br/>
+      Date de sortie: 2010<br/>
       <p>Un peplum sur la mythologie grècque version 21ème siècle (donc quasi intégralement sous fond vert, comme 300)
       avec un casting XXL (Liam Neeson aka Qui-gon Jinn, Ralph Fiennes aka Voldemort, Madds Mikkelsen aka le mec qui cherche
       votre bébé dans Death Stranding et qui pleure du pétrole avec ses yeux..). Scénario assez classique et personnages un peu
       caricaturaux mais dans l’ensemble un bon diversement pour en prendre plein les yeux sans trop se prendre la tête, j’ai bien aimé.<p>
+      </div>
+
+      <div id="card">
+      <img src="source/la_mome.jpeg"><br/>
+      Nom: La Mome<br/>
+      Réalisateur: Olivier Dahan<br/>
+      Date de sortie: 2007<br/>
+      <p>J’ai beaucoup aimé l’ambiance et les décors. Marion Cotillard joue très bien, même si elle est particulièrement irritante
+       lors de la scène où elle apprend la mort de Marcel Cerdan et hurle “MARCEEEEEEEEL” dans le hall de chez elle. Bon film mais un peu déprimant.</p>
+      </div>
+
+      <div id="card">
+      <img src="source/lucy.jpg"><br/>
+      Nom: Lucy<br/>
+      Réalisateur: Luc Besson<br/>
+      Date de sortie: 2014<br/>
+      <p>Peut-être l’un des pires films qui m’ait été donné de voir. Gros casting, Morgan Freeman joue le type plein de sagesse comme d’habitude, 
+      Scarlett Johansson est magnifique et le démarrage est plutôt sympa puis à partir de la scène ou elle se réveille en prison et commence à découvrir ses 
+      pouvoirs tout part en sucette avec des rebondissements toujours plus imporbables et mal amenés. Et puis ce final en apothéose (spoiler : à la fin Scarlett 
+      devient tellement intelligente qu’elle se transforme en clef USB ). Nul de chez nul, à éviter à tout prix.</p>
       </div>
 
     <h1>Critiques Littéraires</h1>
