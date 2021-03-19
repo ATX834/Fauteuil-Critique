67bee9d50b46ef8cdc4b4d6787748808b7f20cc4
Baptistedupont9094 <baptistedupont9094@gmail.com>
2021-03-19 12:21:41 +0100

    background disco mode

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
   <footer>
-    <img class="socialNetworkLogo" src="source/logo-youtube.png" alt= "Bouton Youtube">
-    <img class="socialNetworkLogo" src="source/logo-twitter.png" alt= "Bouton Twitter">
-    <img class="socialNetworkLogo" src="source/logo-facebook.png" alt= "Bouton Facebook">
-    <img class="socialNetworkLogo" src="source/logo-insta.png" alt= "Bouton Instagram">
+    <a href="www.youtube.com"><img class="socialNetworkLogo" src="source/logo-youtube.png" alt= "Bouton Youtube"></a>
+    <a href="www.twitter.com"><img class="socialNetworkLogo" src="source/logo-twitter.png" alt= "Bouton Twitter"></a>
+    <a href="www.facbook.com"><img class="socialNetworkLogo" src="source/logo-facebook.png" alt= "Bouton Facebook"></a>
+    <a href="www.instagram.com"><img class="socialNetworkLogo" src="source/logo-insta.png" alt= "Bouton Instagram"></a>
     <p>
       <a href="form.php" style="color : white;">Contact</a>
     </p>
