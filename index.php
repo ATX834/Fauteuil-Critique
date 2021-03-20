9648255b94ffd5cb425d234b4f3528b47090db28
Baptistedupont9094 <baptistedupont9094@gmail.com>
2021-03-20 20:51:29 +0100

    modified home

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
     <content class="contenuIndex">
 
       <div class="paragrapheIndex">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <h1>"La critique peut être désagréable mais elle est nécessaire. 
+          Elle est comme la douleur pour le corps humain: elle attire l'attention sur ce qui ne va pas."<br/> -- Winston Churchill --
+        </h1>
         <img>
       </div>
 
