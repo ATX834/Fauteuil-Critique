13812d20ad370b604c8430e190df2a7f47ebeabe
Baptistedupont9094 <baptistedupont9094@gmail.com>
2021-03-20 10:24:26 +0100

    modified logo

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
   <footer>
-    <a href="www.youtube.com"><img class="socialNetworkLogo" src="source/logo-youtube.png" alt= "Bouton Youtube"></a>
-    <a href="www.twitter.com"><img class="socialNetworkLogo" src="source/logo-twitter.png" alt= "Bouton Twitter"></a>
-    <a href="www.facbook.com"><img class="socialNetworkLogo" src="source/logo-facebook.png" alt= "Bouton Facebook"></a>
-    <a href="www.instagram.com"><img class="socialNetworkLogo" src="source/logo-insta.png" alt= "Bouton Instagram"></a>
+    <a href="www.whatsapp.com"><img class="socialNetworkLogo" src="source/whatsapp.png" alt= "Bouton Wahtsapp"></a>
+    <a href="www.twitter.com"><img class="socialNetworkLogo" src="source/twitter.png" alt= "Bouton Twitter"></a>
+    <a href="www.facbook.com"><img class="socialNetworkLogo" src="source/facebook.png" alt= "Bouton Facebook"></a>
+    <a href="www.instagram.com"><img class="socialNetworkLogo" src="source/instagram.png" alt= "Bouton Instagram"></a>
     <p>
       <a href="form.php" style="color : white;">Contact</a>
     </p>
