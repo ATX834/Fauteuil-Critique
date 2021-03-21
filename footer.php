8bd885a4db30aefcd952d781208adf88500a20d6
ATX834 <clementbacquet@hotmail.fr>
2021-03-21 11:58:18 +0100

    logo réseaux sociaux amènent à un vrai lien

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
       <img src="source/scroll_to_top.png" alt="Scroll">
     </a>
   </div>
-  
+
   <footer>
     <div class="logo">
-      <a href="www.whatsapp.com"><img class="socialNetworkLogo" src="source/whatsapp.png" alt="Bouton Wahtsapp"></a>
-      <a href="www.twitter.com"><img class="socialNetworkLogo" src="source/twitter.png" alt="Bouton Twitter"></a>
-      <a href="www.facbook.com"><img class="socialNetworkLogo" src="source/facebook.png" alt="Bouton Facebook"></a>
-      <a href="www.instagram.com"><img class="socialNetworkLogo" src="source/instagram.png" alt="Bouton Instagram"></a>
+      <a href="http://www.whatsapp.com"><img class="socialNetworkLogo" src="source/whatsapp.png" alt="Bouton Wahtsapp"></a>
+      <a href="http://www.twitter.com"><img class="socialNetworkLogo" src="source/twitter.png" alt="Bouton Twitter"></a>
+      <a href="https://www.facebook.com"><img class="socialNetworkLogo" src="source/facebook.png" alt="Bouton Facebook"></a>
+      <a href="https://www.instagram.com"><img class="socialNetworkLogo" src="source/instagram.png" alt="Bouton Instagram"></a>
     </div>
     <div>
       <a href="form.php">Contactez moi</a>
