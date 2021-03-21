2e1564881ca1b0431a2a468f6be929832fe72a59
Pierre-Jean Touraille <pierre-jean-touraille_student2021@wilder.school>
2021-03-21 09:52:18 +0100

    ajout de la touche retour en haut

--- footer.php
+++ footer.php
@@ -1,13 +1,21 @@
+  <div id="scroll-to-top">
+    <a href="#top">
+      <img src="source/scroll_to_top.png" alt="Scroll">
+    </a>
+  </div>
+  
   <footer>
     <div class="logo">
-      <a href="www.whatsapp.com"><img class="socialNetworkLogo" src="source/whatsapp.png" alt= "Bouton Wahtsapp"></a>
-      <a href="www.twitter.com"><img class="socialNetworkLogo" src="source/twitter.png" alt= "Bouton Twitter"></a>
-      <a href="www.facbook.com"><img class="socialNetworkLogo" src="source/facebook.png" alt= "Bouton Facebook"></a>
-      <a href="www.instagram.com"><img class="socialNetworkLogo" src="source/instagram.png" alt= "Bouton Instagram"></a>
+      <a href="www.whatsapp.com"><img class="socialNetworkLogo" src="source/whatsapp.png" alt="Bouton Wahtsapp"></a>
+      <a href="www.twitter.com"><img class="socialNetworkLogo" src="source/twitter.png" alt="Bouton Twitter"></a>
+      <a href="www.facbook.com"><img class="socialNetworkLogo" src="source/facebook.png" alt="Bouton Facebook"></a>
+      <a href="www.instagram.com"><img class="socialNetworkLogo" src="source/instagram.png" alt="Bouton Instagram"></a>
     </div>
     <p>
       <a href="form.php" style="color : white;">Contact</a>
     </p>
   </footer>
-</body>
-</html>
+  </body>
+  <script src="script.js"></script>
+
+  </html>
