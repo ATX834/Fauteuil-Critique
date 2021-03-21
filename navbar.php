353ce9b5361ba71295613f16a0d049706c41e911
Pierre-Jean Touraille <pierre-jean-touraille_student2021@wilder.school>
2021-03-21 09:54:27 +0100

    mise a jour visuel et integration du burger menu simplifie

--- navbar.php
+++ navbar.php
@@ -1,12 +1,12 @@
 <nav>
-
-    <div  class="burger">
-        <div id="burger-icon">
-            <button class="button-burger">
-                <img id='burger-menu' src="source/burger-menu-icon.jpg" alt="burger-menu">
-            </button>
-        </div>
-        <div class="burger-click">
+    <div class="navbar-main">
+        <div class="burger">
+            <div id="burger-icon">
+                <button class="button-burger" onclick="openMenu(this)">
+                    <img id='burger-menu' src="source/burger-menu-icon.jpg" alt="burger-menu">
+                </button>
+            </div>
+            <!-- <div class="burger-click">
             <ul class="burger-list">
                 <li>
                     <a href="index.php"> Index
@@ -19,47 +19,62 @@
                 <li><a href="form.php"> Contact
                     </a></li>
             </ul>
+        </div> -->
         </div>
-    </div>
 
-
-    <div class="logo-mobile">
-        <a href="index.php">
-            <img src="source/logo.png" alt="logo" width="80px" height="100%">
-        </a>
-    </div>
-
-    <div class="menu">
-        <div class="home">
+        <div class="logo-mobile">
             <a href="index.php">
-                <img id='logo' src="source/home-icon.png" alt="home-icon" width="50px" height="50px">
+                <img src="source/logo.png" alt="logo" width="80px" height="100%">
             </a>
         </div>
 
-        <div class="review">
-            <a href="reviews.php">
-                <h1> Reviews </h1>
-            </a>
+        <div class="menu">
+            <div class="home">
+                <a href="index.php">
+                    <h1>Home</h1>
+                    <!-- <img id='logo' src="source/home-icon.png" alt="home-icon" width="50px" height="50px"> -->
+                </a>
+            </div>
+            <div class="review">
+                <a href="reviews.php">
+                    <h1>Reviews</h1>
+                </a>
+            </div>
+            <div class="about">
+                <a href="aboutme.php">
+                    <h1>A propos</h1>
+                </a>
+            </div>
+
+            <div class="form">
+                <a href="form.php">
+                    <h1>Contact</h1>
+                </a>
+            </div>
         </div>
 
-        <div class="about">
-            <a href="aboutme.php">
-                <h1>A propos</h1>
-            </a>
-        </div>
-        <div class="form">
-            <a href="form.php">
-                <h1>Contact</h1>
-            </a>
+        <div class="colormode">
+            <label class="theme-switch" for="checkbox">
+                <input type="checkbox" id="checkbox" />
+                <div class="slider round"></div>
+            </label>
+            <img src="source/disco-ball2.png" alt="disco-ball" width="80px" height="80px">
         </div>
     </div>
-
-    <div class="colormode">
-        <label class="theme-switch" for="checkbox">
-            <input type="checkbox" id="checkbox" />
-            <div class="slider round"></div>
-        </label>
-        <img src="source/disco-ball2.png" alt="disco-ball" width="80px" height="80px">
+    <div class="navbar-list">
+        <ul>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="reviews.php">Reviews</a>
+            </li>
+            <li>
+            <a href="aboutme.php">About</a>
+            </li>
+            <li>
+            <a href="form.php">Contact</a>
+            </li>
+        </ul>
     </div>
-    <script src="switch.js"></script>
 </nav>
