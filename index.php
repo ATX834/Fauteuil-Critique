5f04db8f58bd00fb5d987ddabe7fad8c296de4b9
PierreCapel <pecapel@gmail.com>
2021-03-24 12:32:29 +0100

    removed lorem ipsum

--- index.php
+++ index.php
@@ -9,32 +9,6 @@
     </h1>
     <h2>-- Winston CHURCHILL --</h2>    
   </div>
-
-  <div class="paragrapheIndex">
-    <p>Libero id faucibus nisl tincidunt eget nullam non. Habitant morbi tristique senectus et netus et. Adipiscing diam donec adipiscing tristique risus nec feugiat. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Aliquet lectus proin nibh nisl condimentum id venenatis a condimentum. Dictum fusce ut placerat orci nulla pellentesque dignissim enim. Luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Lacus luctus accumsan tortor posuere ac ut consequat semper. Fringilla est ullamcorper eget nulla. Turpis massa sed elementum tempus egestas sed sed.
-    </p>
-    <img>
-  </div>
-
-  <div class="paragrapheIndex">
-    <p>
-      At in tellus integer feugiat scelerisque varius morbi enim nunc. Mi quis hendrerit dolor magna eget est lorem ipsum. Hac habitasse platea dictumst vestibulum. Amet nisl suscipit adipiscing bibendum est ultricies integer. Lobortis elementum nibh tellus molestie. Pharetra magna ac placerat vestibulum lectus. Cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla. Vitae suscipit tellus mauris a. Convallis convallis tellus id interdum.
-    </p>
-    <img>
-  </div>
-
-  <div class="paragrapheIndex">
-    <p>
-      Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Nunc scelerisque viverra mauris in aliquam sem. Suspendisse in est ante in nibh. Maecenas accumsan lacus vel facilisis volutpat est velit egestas dui. Hendrerit dolor magna eget est. Tortor dignissim convallis aenean et tortor at. At tempor commodo ullamcorper a lacus vestibulum sed.
-    </p>
-    <img>
-  </div>
-
-  <div class="paragrapheIndex">
-    <p>Tempor orci dapibus ultrices in iaculis nunc sed augue. Euismod lacinia at quis risus sed vulputate odio ut. Nibh tortor id aliquet lectus proin nibh. Mauris augue neque gravida in fermentum et sollicitudin. Ac placerat vestibulum lectus mauris. Vitae nunc sed velit dignissim sodales ut eu.
-    </p>
-    <img>
-  </div>
   
 </content>
 </main>
